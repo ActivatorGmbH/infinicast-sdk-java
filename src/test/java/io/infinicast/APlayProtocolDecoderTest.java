02ac6e79ce4215c525a3111b86ef438d00829ff0
Michael Cornel <michael@stieler.it>
2024-03-04 18:27:28 +0000

    Migrate test to new Mockito syntax

--- src/test/java/io/infinicast/APlayProtocolDecoderTest.java
+++ src/test/java/io/infinicast/APlayProtocolDecoderTest.java
@@ -6,12 +6,12 @@ import org.apache.mina.filter.codec.ProtocolDecoderOutput;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 /**
